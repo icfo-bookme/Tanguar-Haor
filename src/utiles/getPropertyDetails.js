b3f259a484bd948565d26e74c89c827c8e617fb7
Tahsin411934 <tahsin.mail.0503@gmail.com>
2024-11-30 12:54:07 +0600

    error arise

--- src/utiles/getPropertyDetails.js
+++ src/utiles/getPropertyDetails.js
@@ -1,9 +1,10 @@
-import React from 'react';
 
-const getPropertyDetails = async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/propertySummary`);
-    const data = await response.json();
-    return data; 
+
+const getPropertyDetails = async (id) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/propertySummary/${id}`);
+    const propertyDetails = await response.json();
+
+    return propertyDetails; 
     
 };
 
