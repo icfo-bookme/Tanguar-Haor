fde020c71535fc4246e0a6ba2940067ebe853733
Tahsin411934 <tahsin.mail.0503@gmail.com>
2024-12-05 15:07:24 +0600

    test

--- src/utiles/propertySummary.js
+++ src/utiles/propertySummary.js
@@ -1,15 +1,14 @@
 const propertySummary = async () => {
-
     try {
-        const res = await fetch(`https://freecvbd.com/admin/api/propertySummary`);
-    const data = await res.json();
-    return data; 
+        const res = await fetch(`https://freecvbd.com/admin/api/propertySummary`, {
+            cache: 'no-store', // Ensure no caching
+        });
+        const data = await res.json();
+        return data; 
     } catch (error) {
-        console.log(error);
-        return []
+        console.log('Error fetching property summary:', error);
+        return [];
     }  
-    ////
-    
 };
 
 export default propertySummary;
