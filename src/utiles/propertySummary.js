2505b595cecdc5ea19a4f30a13eabdc1d42367d9
Tahsin411934 <tahsin.mail.0503@gmail.com>
2024-12-05 14:01:30 +0600

    updated

--- src/utiles/propertySummary.js
+++ src/utiles/propertySummary.js
@@ -1,7 +1,7 @@
 const propertySummary = async () => {
 
     try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/propertySummary`);
+        const response = await fetch(`https://freecvbd.com/admin/api/propertySummary`);
     const data = await response.json();
     return data; 
     } catch (error) {
